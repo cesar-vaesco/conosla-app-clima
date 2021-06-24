e8111d965040aaad1719b69ffc3511ab4f33cb78
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-24 16:16:23 -0500

    Usando la API de MAPBOX y generando el token de acceso para poder usar la api

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ###
 https://reqres.in/  --> Pruebe su front-end contra una API real
+
+https://www.mapbox.com/
+
+https://docs.mapbox.com/api/search/geocoding/
--- models/busqueda.js
+++ models/busqueda.js
@@ -12,7 +12,7 @@ class Busquedas {
         // petición http
 
         try {
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid%2C%20Espa%C3%B1a.json?access_token=pk.eyJ1IjoiY2VzYXJkZXYiLCJhIjoiY2txYmUzY2xkMDNuaTJucGUwZ2tmcmc5cSJ9.pth8dYxnio3FOE9u_4S3og&limit=5&language=es');
             console.log(resp.data);
         } catch (error) {
             return [];
