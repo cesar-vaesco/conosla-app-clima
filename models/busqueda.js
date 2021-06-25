654ad6e5844a97084963af75250819cde7a9f881
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-24 20:39:03 -0500

    Instancias de Axios

--- models/busqueda.js
+++ models/busqueda.js
@@ -8,12 +8,28 @@ class Busquedas {
         //TODO: leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiY2VzYXJkZXYiLCJhIjoiY2txYmUzY2xkMDNuaTJucGUwZ2tmcmc5cSJ9.pth8dYxnio3FOE9u_4S3og',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar = '') {
-        // petición http
 
         try {
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid%2C%20Espa%C3%B1a.json?access_token=pk.eyJ1IjoiY2VzYXJkZXYiLCJhIjoiY2txYmUzY2xkMDNuaTJucGUwZ2tmcmc5cSJ9.pth8dYxnio3FOE9u_4S3og&limit=5&language=es');
+            // petición http
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+
+            });
+
+            const resp = await intance.get();
+
             console.log(resp.data);
+
         } catch (error) {
             return [];
         }
